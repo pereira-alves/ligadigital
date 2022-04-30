f37318c92fc4a0f0df50fd0d15deb53a3ae85a8d
Anderson Alves <anderson.aza@gmail.com>
2022-04-30 06:01:31 -0300

    Alterando conteudo

--- projeto-final/front-end/orti-fruti/src/services/api.js
+++ projeto-final/front-end/orti-fruti/src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'http://localhost:3010'
-    //baseURL: process.env.REACT_APP_BASE_URL
+    //baseURL: 'http://localhost:3010'
+    baseURL: process.env.REACT_APP_BASE_URL
 })
 
 export default api;
